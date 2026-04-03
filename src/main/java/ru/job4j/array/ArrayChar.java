cf06297a5a473cf9b02007422652df7234908e26
Kristy2594 <k.primakova2017@yandex.com>
2026-04-03 21:54:42 +0300

    Слово начинается с ... [#505233 #561386]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,7 +2,6 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] prefix) {
-        boolean result = true;
         for (int index = 0; index < prefix.length; index++) {
             if (prefix[index] != word[index]) {
                 return false;
