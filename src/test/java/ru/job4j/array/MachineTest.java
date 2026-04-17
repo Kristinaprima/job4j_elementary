9168536489a702219f75e97df9c30188e366df64
Kristy2594 <k.primakova2017@yandex.com>
2026-04-17 18:10:04 +0300

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#505250]

--- src/test/java/ru/job4j/array/MachineTest.java
+++ src/test/java/ru/job4j/array/MachineTest.java
@@ -36,7 +36,7 @@ public class MachineTest {
     public void whenMoney50Price32() {
         int money = 50;
         int price = 32;
-        int[] result = Machine.change(money, price);
+        int[] result = Machine.change(money,price);
         int[] expected = {10, 5, 2, 1};
         assertThat(result).containsExactly(expected);
     }
